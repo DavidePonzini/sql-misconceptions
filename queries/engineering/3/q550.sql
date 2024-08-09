b1183bb1eac10dd58d80dce4f60da9adfb6ede80
Hamid Livani <65550422+hamidlvn@users.noreply.github.com>
2024-08-10 00:41:18 +0200

    Some syntax edit

--- queries/engineering/3/q550.sql
+++ queries/engineering/3/q550.sql
@@ -3,5 +3,5 @@ FROM Prodotto p
 LEFT JOIN DettaglioOrdine d ON p.idProd = d.idProd
 LEFT JOIN Ordine o ON d.idOrd = o.idOrd
 LEFT JOIN Cliente c ON o.idClient = c.idClient
-WHERE c.città IS NULL 
-   OR c.città NOT IN ('Venezia', 'Brescia');
+WHERE c.città IS NULL 
+   OR c.città NOT IN ('Venezia', 'Brescia');
